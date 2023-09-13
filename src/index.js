843c59e7107115430e2c90fa1e203300b3f310d0
LilitVardanyan1992 <lvardanyan890@gmail.com>
2023-09-13 05:09:05 -0700

    created mainCategories section, with full responsive code

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import "./scss/styles.scss";
 import swiper from "./js/header/miniHeaderSlider";
 import burgerMenu from "./js/header/burgerMenu";
 import "./img/main-info.png";
+import "./img/gotovie-nobori.jpg";
+import "./img/corporative.jpg";
+import "./img/nabor-individual.jpg";
+import "./img/optovie.jpg";
+import "./img/svadebni.jpg";
+import "./img/sobrat-svoy-nabor.jpg";
 
-burgerMenu()
+burgerMenu();
